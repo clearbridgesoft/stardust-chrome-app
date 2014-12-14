93952e863461a1e4a7c2d63c983de8edfacf7452
stan wang <swang@rawideas.com>
2014-12-14 17:37:42 +1100

    fix(MUC-22): replace default hisotry.back() with just going to home page

--- js/app.js
+++ js/app.js
@@ -149,7 +149,7 @@ define(function(require){
 						$.mobile.navigate(data.target,data.payload);
 					}
 					else{
-						$.mobile.back();
+						window.location.hash="#mainPage";
 					}
 				});
 				
